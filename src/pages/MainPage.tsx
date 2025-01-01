09195ee9c62ea5825e6efd67bd896983f9b12669
sorata000x <70079892+sorata000x@users.noreply.github.com>
2025-01-01 12:58:31 -0800

    Implemented onboarding page and google signup

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,291 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { addNote, fetchNotes, updateNote } from '../api/notes';
+import {v4 as uuid} from 'uuid';
+import { hybridSearch } from '../api/search';
+import generateEmbedding, { chatWithNotes } from '../api/openai';
+import ReactMarkdown from 'react-markdown';
+import { IoIosArrowBack } from "react-icons/io";
+import { FaRegUserCircle } from "react-icons/fa";
+import { supabase } from '../lib/supabase';
+import { User } from '@supabase/supabase-js';
+import '../App.css';
+
+type Message = {
+  id: string;
+  content: string;
+  created_at: string; // ISO date string
+  role: string;
+};
+
+export function MainPage() {
+  const [editing, setEditing] = useState<Message | null>(null);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [user, setUser] = useState<User | null>(null);
+  const [profilePicture, setProfilePicture] = useState<string | null>(null);
+
+  const sidePanelStyle = {
+    width: '560px', // Set the width to match Chrome's side panel size
+    height: '100vh', // Full viewport height
+    backgroundColor: "#212121",
+  };
+
+  const update = async () => {
+    setMessages(await fetchNotes());
+    // User picture
+    if (user) {
+      const picture = user.user_metadata.avatar_url || user.user_metadata.picture;
+      setProfilePicture(picture || null);
+    }
+  }
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    update();
+  }, [user]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight; // Scroll to the bottom
+    }
+  }, [editing, messages])
+
+  async function handleAddNote(role: string, content: string) {
+    const id = uuid();
+    const created_at = new Date().toISOString();
+    const embedding = await generateEmbedding(content);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        id,
+        content,
+        created_at,
+        embedding,
+        role,
+      },
+    ]);
+    await addNote({
+      role,
+      content,
+      embedding
+    });
+  }
+
+  async function handleUpdateNote(id: string, content: string) {
+    const newMessages = messages.map((m) =>
+      m.id === id ? { ...m, content: content } : m
+    );
+    setMessages(newMessages);
+    await updateNote(id, {content});
+  }
+
+  /* async function updateNote(id: string, content: string) {
+    const response = await fetch('/api/notes', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id, content }),
+    });
+
+    if (!response.ok) {
+      alert('Error updating note');
+    }
+  }
+
+  async function deleteNote(id: string) {
+    const response = await fetch('/api/notes', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id }),
+    });
+
+    if (!response.ok) {
+      alert('Error deleting note');
+    }
+  } */
+
+  async function handleHybridSearch(query: string) {
+    const embedding = await generateEmbedding(query);
+    const data = await hybridSearch(query, embedding);
+    return data;
+  }  
+
+  async function handleChat(input: string) {
+    const notes = await handleHybridSearch(input);
+    const response = await chatWithNotes(input, notes);
+    if(!response) return;
+    handleAddNote('assistant', response)
+  }
+
+  const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const input = (e.target as HTMLInputElement).value; // Cast e.target
+      if (input) {
+        // Search notes if first two characters are spaces
+        if (input.startsWith(' ')) {
+          await handleAddNote('user', input.trim());
+          handleChat(input.trim());
+        } else {
+          handleAddNote('user', input);
+        }
+        setEditing(null); // Close note editor
+        (e.target as HTMLInputElement).value = ""; // Clear the input field
+      }
+    }
+  };  
+
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') {
+        console.log('User signed in:', session?.user);
+        setUser(session?.user || null);
+      }
+    });
+    
+    // To unsubscribe:
+    return () => data.subscription.unsubscribe();    
+  }, []);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const status = params.get('status');
+    
+    if (status === 'signed_in') {
+      console.log('User has just signed up!');
+      // Handle signed-in status
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data?.status === 'signed_in') {
+        console.log('User has signed up:', event.data.user);
+        setUser(event.data.user || null);
+        // Handle signed-in user, e.g., update UI
+      }
+    };
+  
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
+
+  const handleOpenOnboarding = () => {
+    // Define the URL of the onboarding page
+    const onboardingURL = chrome.runtime.getURL("onboarding.html");
+
+    // Open a popup window
+    window.open(
+      onboardingURL,
+      "onboarding", // Popup window name
+      "width=400,height=600,scrollbars=yes,resizable=yes"
+    );
+  };
+
+  return (
+    <div className='flex justify-start items-start w-full bg-blue-500'>
+      <div style={sidePanelStyle} className="flex flex-col h-full">
+      <div 
+        ref={containerRef}
+        className="pt-3 pb-5 flex-grow overflow-auto flex flex-col scrollbar scrollbar-thumb-blue-500 scrollbar-track-gray-300">
+        {editing ? 
+          <NoteEdit content={editing.content} onChange={(e) => {handleUpdateNote(editing.id, e.target.value)}} close={() => {setEditing(null)}}/> : 
+          <NoteChat messages={messages} onNoteClick={(note) => {setEditing(note)}}/>}
+      </div>
+      <div className="p-3 pt-0 pb-0">
+        <input 
+          className="h-12 flex-shrink-0 w-full rounded-lg focus:outline-none p-3"
+          style={{backgroundColor: "#2f2f2f"}}
+          onKeyDown={handleKeyDown}
+          />
+        <div className='flex justify-end p-3'>
+          {profilePicture ? 
+            <button 
+              className="h-6 w-6 rounded-full cursor-pointer bg-cover bg-center" 
+              style={{backgroundImage: `url(${profilePicture})`}}
+              onClick={handleOpenOnboarding}
+              />
+            :
+            <FaRegUserCircle 
+              className="h-6 w-6 rounded-full cursor-pointer" 
+              onClick={handleOpenOnboarding}
+            />
+          }
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+type MessageProps = {
+  key: string;
+  content: string;
+};
+
+const AssistantMessage = ({content}: MessageProps) => {
+  return (
+    <div className='flex'>
+      <div className='w-2'>
+        <div style={{ width: "4px", height: "100%", backgroundColor: "#525252"}} />
+      </div>
+      <div className="p-2 pt-2 pb-2 w-full">
+        <ReactMarkdown>{content}</ReactMarkdown>
+      </div>
+    </div>
+  )
+}
+
+const UserMessage = ({content, onClick}: {content: string, onClick: () => void}) => {
+  return (
+    <div className="p-4 pt-2 pb-2 w-full user-message cursor-pointer" onClick={onClick}>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
+  )
+}
+
+const NoteChat = ({messages, onNoteClick}: {messages: Message[], onNoteClick: (m: Message) => void}) => {
+  return <>
+    {
+      messages.map((m) => {
+        if (m.role == 'user') {
+          return <UserMessage key={uuid()} content={m.content} onClick={() => onNoteClick(m)}/>
+        } 
+        if (m.role == 'assistant') {
+          return <AssistantMessage key={uuid()} content={m.content} />
+        }
+      })
+    }
+  </>
+}
+
+const NoteEdit = ({content, onChange, close}: {content: string, onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void, close: () => void}) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [value, setValue] = useState<string>(content);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.focus(); // Autofocus
+      textareaRef.current.selectionStart = textareaRef.current.value.length; // Set cursor position to the end
+      textareaRef.current.selectionEnd = textareaRef.current.value.length;
+    }
+  }, []);
+  
+  return (
+    <div className='flex flex-col h-full'>
+      <IoIosArrowBack className='m-1 ml-3 mb-0 cursor-pointer' onClick={() => close()} size={22}/>
+      <textarea
+        ref={textareaRef}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e);
+        }}
+        placeholder='tesrt'
+        className='h-full bg-transparent focus:outline-none p-4 pt-2 pb-2'
+        style={{
+          boxSizing: 'border-box',
+          resize: 'none',
+        }}
+      />
+    </div>
+  )
+}
