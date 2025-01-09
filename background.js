407ae62d59e9ff0e3d4fc5cc22b32ce8419f030b
Aiden Chen <wdragon000x@gmail.com>
2025-01-09 12:21:14 -0800

    Fix open side panel when click on icon

--- background.js
+++ background.js
@@ -1,26 +1,26 @@
-/* chrome.sidePanel
+chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
 
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: 'openSidePanel',
-      title: 'Open side panel',
-      contexts: ['all']
-    });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'openSidePanel',
+    title: 'Open side panel',
+    contexts: ['all']
   });
-  
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === 'openSidePanel') {
-      // This will open the panel in all the pages on the current window.
-      chrome.sidePanel.open({ windowId: tab.windowId });
-    }
-  }); */
+});
 
-  chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === "install") {
-      // Open the onboarding page in a new tab
-      chrome.tabs.create({ url: "onboarding.html" });
-    }
-  });
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'openSidePanel') {
+    // This will open the panel in all the pages on the current window.
+    chrome.sidePanel.open({ windowId: tab.windowId });
+  }
+});
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === "install") {
+    // Open the onboarding page in a new tab
+    chrome.tabs.create({ url: "onboarding.html" });
+  }
+});
   
